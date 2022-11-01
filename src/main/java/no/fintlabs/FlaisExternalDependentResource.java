1a03490f3c58b75e7c5eb6c6174c6360e51536f8
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-11-01 18:52:54 +0100

    Fixed javadocs we hope

--- src/main/java/no/fintlabs/FlaisExternalDependentResource.java
+++ src/main/java/no/fintlabs/FlaisExternalDependentResource.java
@@ -8,13 +8,14 @@ import io.javaoperatorsdk.operator.processing.dependent.external.PerResourcePoll
 
 /**
  * Representing an external dependent resource in FLAIS. This can e.g. be an Azure Storage account.
+ * <p>
+ * It automatically polls the external source every 5 second to get the latest version. You can
+ * change the polling interval by setting the {@code setPollingPeriod} in the constructor. If this resource is
+ * dependent of any other dependent resources you should call {@code dependsOn} in the constructor.
  *
  * @param <T> the class providing external model.
  * @param <C> the class providing the CRD type.
  * @param <S> the class providing the spec of the CRD.
- * @implNote It automatically polls the external source every 5 second to get the latest version. You can
- * change the polling interval by setting the {@code setPollingPeriod} in the constructor. If this resource is
- * dependent of any other dependent resources you should call {@code dependsOn} in the constructor.
  */
 public abstract class FlaisExternalDependentResource<T, C extends FlaisCrd<S>, S extends FlaisSpec> extends PerResourcePollingDependentResource<T, C>
         implements EventSourceProvider<C>,
--- src/main/java/no/fintlabs/FlaisKubernetesDependentResource.java
+++ src/main/java/no/fintlabs/FlaisKubernetesDependentResource.java
@@ -9,12 +9,12 @@ import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernete
 
 /**
  * Represents a Kubernetes object.
+ * <p>
+ * If this resource is dependent of any other dependent resources you should call {@code dependsOn} in the constructor.
  *
  * @param <T> the class providing a kubernetes model.
  * @param <C> the class providing the CRD type.
  * @param <S> the class providing the spec of the CRD.
- * @implNote If this resource is
- * dependent of any other dependent resources you should call {@code dependsOn} in the constructor.
  */
 public abstract class FlaisKubernetesDependentResource<T extends HasMetadata, C extends FlaisCrd<S>, S extends FlaisSpec> extends CRUDKubernetesDependentResource<T, C> {
 
