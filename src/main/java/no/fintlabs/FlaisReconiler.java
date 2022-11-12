9453169a751d15f2f6b0e265ebcb499c501a8958
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-11-12 18:47:34 +0100

    FLAIS-25 Moved ReconcileHandler method to FlaisReconiler and removed the interface. In addtion if it is a create we update not only the status but the whole object.

--- src/main/java/no/fintlabs/FlaisReconiler.java
+++ src/main/java/no/fintlabs/FlaisReconiler.java
@@ -3,11 +3,13 @@ package no.fintlabs;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Deleter;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.ReconcileResult;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Workflow;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.WorkflowReconcileResult;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -23,8 +25,7 @@ public abstract class FlaisReconiler<T extends FlaisCrd<S>, S extends FlaisSpec>
         implements Reconciler<T>,
         Cleaner<T>,
         ErrorStatusHandler<T>,
-        EventSourceInitializer<T>,
-        ReconcileHandler<FlaisStatus, T, S> {
+        EventSourceInitializer<T> {
 
     private final FlaisWorkflow<T, S> workflow;
     private final List<? extends DependentResource<?, T>> eventSourceProviders;
@@ -50,6 +51,43 @@ public abstract class FlaisReconiler<T extends FlaisCrd<S>, S extends FlaisSpec>
         WorkflowReconcileResult reconcile = fileShareWorkflow.reconcile(resource, context);
 
         return updateStatus(reconcile, resource, new FlaisStatus());
+    }
+
+    public UpdateControl<T> updateStatus(WorkflowReconcileResult reconcile, T resource, FlaisStatus status) {
+        if (isCreated(reconcile)) {
+            resource.setStatus(createStatus(reconcile, status));
+            return UpdateControl.updateResourceAndStatus(resource);
+        }
+
+        if (hasUpdates(reconcile)) {
+
+            resource.setStatus(createStatus(reconcile, status));
+            return UpdateControl.patchStatus(resource);
+        }
+
+        return UpdateControl.noUpdate();
+    }
+
+    public boolean hasUpdates(WorkflowReconcileResult reconcile) {
+        return reconcile.getReconcileResults().values().stream().map(reconcileResult -> reconcileResult.getSingleOperation().name())
+                .filter(s -> s.equals(ReconcileResult.Operation.NONE.name()))
+                .count() != reconcile.getReconciledDependents().size();
+    }
+
+    public boolean isCreated(WorkflowReconcileResult reconcile) {
+        return reconcile.getReconcileResults().values().stream().map(reconcileResult -> reconcileResult.getSingleOperation().name())
+                .filter(s -> s.equals(ReconcileResult.Operation.CREATED.name()))
+                .count() == reconcile.getReconciledDependents().size();
+    }
+
+    public FlaisStatus createStatus(WorkflowReconcileResult reconcile, FlaisStatus status) {
+        List<String> results = new ArrayList<>();
+        reconcile.getReconcileResults()
+                .forEach((dependentResource, reconcileResult) -> results.add(dependentResource.resourceType().getSimpleName() + " -> " + reconcileResult.getSingleOperation().name()));
+        status.setDependentResourceStatus(results);
+
+        return status;
+
     }
 
     @Override
