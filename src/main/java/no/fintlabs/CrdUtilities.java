a0e878bff7c8dd2c8af9162e09743e04d143a8d0
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2023-11-28 15:02:32 +0100

    Added CrdUtilities, useful to get annotation by key

---
+++ src/main/java/no/fintlabs/CrdUtilities.java
@@ -0,0 +1,11 @@
+package no.fintlabs;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+
+import java.util.Optional;
+
+public class CrdUtilities {
+    public static Optional<String> getValueFromAnnotationByKey(HasMetadata crd, String key) {
+        return Optional.ofNullable(crd.getMetadata().getAnnotations().get(key));
+    }
+}
